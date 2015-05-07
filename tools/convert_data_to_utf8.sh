f1344a5216c757d8c94cfc88ee3c4f4087d02a03
lisency <zh-y-sh@qq.com>
2015-05-07 15:45:51 +0800

    Information Technology dir to InformationTechnology dir

--- tools/convert_data_to_utf8.sh
+++ tools/convert_data_to_utf8.sh
@@ -1,4 +1,14 @@
 
-for file in $(find ../data -name "*.txt"); do
-    iconv -f cp936 -t utf-8 "$file" -o ${file} || (echo "${file} fail";exit 1)
-done
+function convert_multi_file()
+{
+    for file in $(find ../data -name "*.txt"); do
+        iconv -f cp936 -t utf-8 "$file" -o ${file}
+        if [ $? -ne 0 ]
+        then
+            echo "${file} fail"
+            return 1;
+        fi
+    done
+}
+
+convert_multi_file
